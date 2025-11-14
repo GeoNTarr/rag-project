fa3b8073d18a36248396a89de9488f86d0fe0e6b
s0195835 <sooverspecial@gmail.com>
2025-11-14 23:41:54 +0300

    Add files via upload

--- src/data/data_loader.py
+++ src/data/data_loader.py
@@ -1,2 +1,16 @@
-class DataLoader: 
-    pass 
+from typing import List
+import os
+from sentence_transformers import SentenceTransformer
+from contracts import EmbeddingContract
+
+class DataLoader(EmbeddingContract):
+    # Предполагается, что папка с моделью называется sentence-transformer и расположена в папке проекта
+    def get_free_embedding_model(self, model_path="./sentence-transformer"):
+        return SentenceTransformer(model_path)
+
+    def get_available_models(self) -> List[str]:
+        return ["ai-forever/sbert_large_nlu_ru"]
+
+
+
+
